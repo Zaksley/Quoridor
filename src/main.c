d34759e80514725ec693dd2f52f6109705a26ad1
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-03-12 17:17:20 +0100

    keeping files

--- src/main.c
+++ src/main.c
@@ -4,6 +4,15 @@
 
 
 /*
+*   Contains every special function of a player
+*/
+struct player_func
+{
+    char* (*get_name)();
+    struct move_t (*player_play) (struct move_t);
+}; 
+
+/*
 * Get functions from a special player with a dynamic lib.so 
 * 
 * @ Param : Need 2 names of the players (libs)
@@ -14,27 +23,29 @@
 // TODO => Take 2 arguments (lib1 & lib2) to get all functions
 void get_functions()
 {
+
+    struct player_func player1; 
+
     void * handle;
     handle = dlopen("./libplayer.so", RTLD_LAZY);
-    
-    //void (*print_name) (const char *);  
+
     char* (*get_name)(); 
+    struct move_t (*player_play) (struct move_t); 
 
     if (!handle) {
         fprintf(stderr, "%s\n", dlerror());
         exit(EXIT_FAILURE);
     }
 
-     
-
     dlerror(); // Clear any existing error 
 
-    
-    //*(void **) (&print_name) = dlsym(handle, "get_print_name");
     *(void **) (&get_name) = dlsym(handle, "get_player_name");
+    *(void **) (&player_play) = dlsym(handle, "play"); 
 
-    //print_name("Jean"); 
     printf("%s\n", get_name());
+
+    player1.get_name = get_name; 
+    player1.player_play = player_play;
 
     dlclose(handle);
     

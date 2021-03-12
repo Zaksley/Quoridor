a7c48c929b06c61d71a767e09fc910aa5da8b4da
MEDINA Enzo <emedina@enseirb-matmeca.fr>
2021-03-12 14:11:46 +0100

    first commit with 3 files .h

---
+++ move.h
@@ -0,0 +1,36 @@
+#ifndef _QUOR_MOVE_H_
+#define _QUOR_MOVE_H_
+
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/* Players */
+enum color_t { BLACK=0, WHITE=1, NO_COLOR=2 };
+
+/* Types of moves */
+enum movetype_t { WALL=0, MOVE=1, NO_TYPE=2 };
+
+/* Ids of vertices are size_t between 0 and the number of vertices of - 1 */
+
+struct edge_t { size_t fr, to; }; // The ids of two vertices
+
+/* Positions */
+struct move_t {
+  size_t m;           // The id of a vertex
+  struct edge_t e[2]; // The two edges for the wall, set to -1 if not applicable
+  enum movetype_t t;  // The type of the move
+  enum color_t c;     // The color of the player
+};
+
+/* A special edge used to specify that this edge should not be used */
+static inline struct edge_t no_edge() {
+  return (struct edge_t) {SIZE_MAX, SIZE_MAX};
+}
+
+/* A matcher for the special edges */
+static inline int is_no_edge(const struct edge_t e) {
+  return (e.fr == SIZE_MAX) && (e.to == SIZE_MAX);
+}
+
+#endif // _QUOR_MOVE_H_
---
+++ player.h
@@ -0,0 +1,50 @@
+#ifndef _QUOR_PLAYER_H_
+#define _QUOR_PLAYER_H_
+
+#include <stdlib.h>
+
+#include "move.h"
+#include "graph.h"
+
+/* Access to player informations
+ * RETURNS:
+ * - the player name as an [a-zA-Z0-9 -_]* string
+ */
+char const* get_player_name();
+
+/* Player graph initialization
+ * PARAM:
+ * - graph : the graph where the game is played
+ * PRECOND:
+ * - `graph` is a heap-allocated copy of the graph where the game is
+ *   played, that must be freed in the end
+ * - initialize_graph has never been called before
+ */
+void initialize_graph(struct graph_t* graph);
+
+/* Player color initialization
+ * PARAM:
+ * - id : the color assigned to the player
+ * PRECOND:
+ * - id is either BLACK or WHITE
+ * - initialize_color has never been called before
+ */
+void initialize_color(enum color_t id);
+
+/* Computes next move
+ * PARAM:
+ * - previous_move: the move from the previous player
+ * RETURNS:
+ * - the next move for the player.
+ */
+struct move_t play(struct move_t previous_move);
+
+/* Announces the end of the game to the player, and cleans up the
+   memory he may have been using.
+ * POSTCOND:
+ * - every allocation done during the calls to initialize and play
+ *   functions must have been freed
+ */
+void finalize();
+
+#endif // _QUOR_PLAYER_H_

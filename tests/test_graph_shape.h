ed4d3449201f6ce6fb8e41b7ce239ad4ac9806cc
BRASSART Mathieu <Mathieu.Brassart@bordeaux-inp.fr>
2021-05-07 18:15:38 +0200

    added test shape torus

---
+++ tests/test_graph_shape.h
@@ -0,0 +1,10 @@
+#ifndef _TEST_GRAPH_SHAPE_H_
+#define _TEST_GRAPH_SHAPE_H_
+
+#include "../src/graph_modif.h"
+
+void test__shape();
+
+int test__torus_existing_vertex(size_t n);
+
+#endif //_TEST_GRAPH_SHAPE_H_
